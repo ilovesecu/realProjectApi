29c6b8cce0d7eab97e1f7bac9553319723b13df0
ilovesecu <ilovesecu@gmail.com>
2023-06-08 23:12:40 +0900

    FileUpload #1

---
+++ src/main/java/com/playground/real_project_api/file/controller/FileController.java
@@ -0,0 +1,8 @@
+package com.playground.real_project_api.file.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/api/storage")
+public class FileController {
+
+}
