d55fbe70f28ba69daa36a0e3c26376ebf0d50ab5
ilovesecu <ilovesecu@gmail.com>
2023-06-12 01:06:35 +0900

    FileUpload #3 파일 업로드 부분 작업중

--- src/main/java/com/playground/real_project_api/file/service/FileService.java
+++ src/main/java/com/playground/real_project_api/file/service/FileService.java
@@ -1,10 +1,21 @@
 package com.playground.real_project_api.file.service;
 
+import com.playground.real_project_api.commons.val.ServiceType;
+import com.playground.real_project_api.file.val.MsgDescription;
+import com.playground.real_project_api.file.val.RtnResult;
 import com.playground.real_project_api.file.vo.FileUploadParam;
+import com.playground.real_project_api.file.vo.FilesUploadRsp;
 import com.playground.real_project_api.proc.RealProjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -18,8 +29,21 @@ public class FileService {
      * @작성자 : 정승주
      * @변경이력 :
      **********************************************************************************************/
-    public void uploadImage(String type, String subType, FileUploadParam fileUploadParam){
+    public FilesUploadRsp uploadImage(String type, String subType, FileUploadParam fileUploadParam){
+        FilesUploadRsp filesUploadRsp = new FilesUploadRsp();
+
+        MultipartFile[] imageFiles = fileUploadParam.getFiles();
+        //File 예외처리 - custom validate 써보기
+        if(imageFiles == null || imageFiles.length ==0){
+            filesUploadRsp.setCode(RtnResult.DENIED_PARAM.getCode());
+            filesUploadRsp.setMessage(MsgDescription.PARAM_NOT_FOUND.getMessage());
+            return filesUploadRsp;
+        }
+
+
+
         this.uploadFile();
+        return filesUploadRsp;
     }
 
     /**********************************************************************************************
@@ -30,5 +54,37 @@ public class FileService {
      **********************************************************************************************/
     public void uploadFile(){
 
+    }
+    
+    /********************************************************************************************** 
+     * @Method 설명 : 파일 업로드 폴더를 위한 현재 날짜,시,분,초를 key로 하는 Map 반환
+     * @작성일 : 2023-06-11 
+     * @작성자 : 정승주
+     * @변경이력 : 
+     **********************************************************************************************/
+    private Map<String,String> getDateMap(){
+        Map<String,String> dateMap = new HashMap<>();
+        Date date = new Date();
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        String formatted = sdf.format(date);
+        String[] splited = formatted.split("-");
+        dateMap.put("yyyy", splited[0]);
+        dateMap.put("MM", splited[1]);
+        dateMap.put("dd", splited[2]);
+        dateMap.put("HH", splited[3]);
+        dateMap.put("mm", splited[4]);
+        dateMap.put("ss", splited[5]);
+
+        // 1~8:0 / 9~16:1 / 17~0:2
+        int hhIntVal = Integer.parseInt(splited[3]);
+        if(1 <= hhIntVal && hhIntVal <= 8){
+            dateMap.put("hhVal","0");
+        }else if(9 <= hhIntVal && hhIntVal <= 16){
+            dateMap.put("hhVal","1");
+        }else{
+            dateMap.put("hhVal","2");
+        }
+
+        return dateMap;
     }
 }
--- src/main/java/com/playground/real_project_api/file/val/MsgDescription.java
+++ src/main/java/com/playground/real_project_api/file/val/MsgDescription.java
@@ -3,16 +3,17 @@ package com.playground.real_project_api.file.val;
 public enum MsgDescription {
     SUCEESS("업로드 성공"),
     DB_ERROR("DB 연동실패"),
+    PARAM_NOT_FOUND("필수 파라미터 누락"),
     FAIL("업로드 실패");
 
 
-    private String description;
+    private String message;
 
     MsgDescription(String description){
-        this.description = description;
+        this.message = description;
     }
 
-    public String getDescription() {
-        return description;
+    public String getMessage() {
+        return message;
     }
 }
